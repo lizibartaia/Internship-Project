220baf820a863e974a104f0b4b661192449ece6f
Nini Bartaia <ninibartaia5@gmail.com>
2024-07-26 14:09:36 +0400

    initial commmit

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter-buttons.component.html',
+  styleUrl: './counter-buttons.component.scss'
+})
+export class CounterButtonsComponent {
+
+}
---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-counter-output',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter-output.component.html',
+  styleUrl: './counter-output.component.scss'
+})
+export class CounterOutputComponent {
+
+  constructor(){}
+
+}
